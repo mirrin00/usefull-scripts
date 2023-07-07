8db494b866b6755c61e9e82000eb0cd1bf486b8e
mirrin00 <mirrin1905@outlook.com>
2023-07-07 17:03:01 +0300

    Fixed bugs

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 sudo apt update
-sudo apt install -y vim git qemu cmake build-essential manpages-dev
+sudo apt upgrade -y
+sudo apt install -y vim git qemu cmake build-essential manpages-dev curl
 sudo snap install code --classic  # install vscode
 # install yocto deps
 sudo apt install -y gawk \
@@ -36,4 +37,6 @@ sudo apt install -y gawk \
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh ./get-docker.sh
 # Install docker-compose
-curl -SL https://github.com/docker/compose/releases/download/v2.19.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.19.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo usermod -aG docker $USER
+sudo usermod -aG docker $NEW_USER
