c427ea116c9ab40186d1cd5054c37f877c04b887
mirrin00 <mirrin1905@outlook.com>
2023-07-07 16:24:11 +0300

    Added script for yocto roject

---
+++ README.md
@@ -0,0 +1,1 @@
+# Repository with usefull scripts
---
+++ install_dependencies.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+sudo apt update
+sudo apt install -y vim git qemu cmake build-essential manpages-dev
+sudo snap install code --classic  # install vscode
+# install yocto deps
+sudo apt install -y gawk \
+     wget \
+     git \
+     diffstat \
+     unzip \
+     texinfo \
+     gcc \
+     build-essential \
+     chrpath \
+     socat \
+     cpio \
+     python3 \
+     python3-pip \
+     python3-pexpect \
+     xz-utils \
+     debianutils \
+     iputils-ping \
+     python3-git \
+     python3-jinja2 \
+     libegl1-mesa \
+     libsdl1.2-dev \
+     python3-subunit \
+     mesa-common-dev \
+     zstd \
+     liblz4-tool \
+     file \
+     locales
+
+# Install docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh ./get-docker.sh
+# Install docker-compose
+curl -SL https://github.com/docker/compose/releases/download/v2.19.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
